d033db55f08bcbf1b0a80af7092794c3df355bf7
HardRic501 <hardric501@gmail.com>
2018-03-22 15:24:50 +0100

    fixed new answer

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ import datetime
 
 def read_csv(filepath):
     try:
-        with open(filepath) as csvfile:
+        with open(filepath, encoding='utf-8') as csvfile:
             readCSV = csv.DictReader(csvfile, delimiter=',')
             read_list = []
             for row in readCSV:
@@ -15,6 +15,11 @@ def read_csv(filepath):
 
 
 def write_csv(filepath, header, row):
+    for k, v in row.items():
+        if isinstance(v, str):
+            row[k] = v.replace('\r', '').replace('\n', '<br>')
+            
+    
     with open(filepath, 'a', newline='', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames=header, delimiter=',')
         writer.writerow(row)
