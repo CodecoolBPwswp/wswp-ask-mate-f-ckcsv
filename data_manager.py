de689fe4f71ae1a7d82a2f73df455194223b50cf
laszloOrha <orha.laszlo.93@gmail.com>
2018-03-22 11:37:04 +0100

    order completed in desc order

--- data_manager.py
+++ data_manager.py
@@ -60,7 +60,10 @@ def read_question_by_id(id):
 
 
 def ordered(question_list, key, desc=True):
-    return question_list.sort(key=lambda x: x[key], reverse=desc)
+    try:
+        return question_list.sort(key=lambda x: int(x[key]), reverse=desc)
+    except ValueError:
+        return question_list.sort(key=lambda x: x[key], reverse=desc)
 
 
 def delete_questions():
@@ -90,5 +93,5 @@ def read_answers_correct_format(ordered_by=None):
     for answer in answer_list:
         answer["submisson_time"] = datetime.datetime.fromtimestamp(int(answer["submisson_time"])).strftime(
                 '%Y-%m-%d %H:%M:%S')
-        ordered(answer_list, ordered_by )
+        ordered(answer_list, ordered_by)
     return answer_list
