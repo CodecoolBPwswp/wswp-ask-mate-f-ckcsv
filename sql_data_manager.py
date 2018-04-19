b319692c8a7a455a3964985aefbad6802db8d884
Papp Gergely <pappgergo096@gmail.com>
2018-04-19 10:21:01 +0200

    question listing fixed

--- sql_data_manager.py
+++ sql_data_manager.py
@@ -20,7 +20,7 @@ def read_questions(cursor, order, ascdesc):
 @database_common.connection_handler
 def read_question_by_id(cursor, id):
     cursor.execute("""
-         SELECT submisson_time, title, message, username FROM "question"
+         SELECT "question".id, submisson_time, title, message, username FROM "question"
          INNER JOIN "user" ON "question".user_id = "user".id
          WHERE "question".id = %(id)s
     """, {'id': id})
