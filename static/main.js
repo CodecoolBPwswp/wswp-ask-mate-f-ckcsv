420319c7eafeb37b4059a025582b0518f0d81188
Papp Gergely <pappgergo096@gmail.com>
2018-04-19 08:36:58 +0200

    login system

--- static/main.js
+++ static/main.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
+//Handle registration/login form
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const button = document.querySelector("#user_input");
+const form_length = document.querySelectorAll("form")[1].getElementsByTagName("input").length;
+let valid_inputs = [];
 
-    $(".show_comment").on("click", function () {
-        if ($('.new_comment').css("display") == "none"){
-            $('.new_comment').css("display", "block")
-        } else {
-            $('.new_comment').css("display", "none")
-        }
-    })
-});
+username.addEventListener("keyup", checkFields);
+password.addEventListener("keyup", checkFields);
+
+function checkFields(e) {
+	let id = "#"+e.target.getAttribute("id");
+
+	if(document.querySelector(id).value.length >= 6 && !valid_inputs.includes(e.target.getAttribute("id"))){
+		valid_inputs.push(e.target.getAttribute("id"));
+	} else if(document.querySelector(id).value.length <= 6 && valid_inputs.includes(e.target.getAttribute("id"))) {
+		let index = valid_inputs.indexOf(e.target.getAttribute("id"));
+		if (index >= 0) {
+		  valid_inputs.splice( index, 1 );
+		}
+
+	}
+
+	if(valid_inputs.length == form_length) {
+		button.disabled = false;
+	} else {
+		button.disabled = true;
+	}
+
+}
